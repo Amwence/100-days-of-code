6c32856cd12b0f7bf8cdb56f99b7f973cefaddfc
Amwence <Amwence@users.noreply.github.com>
2022-05-09 23:11:18 -0500

    update

--- Day 9/Silent Auction.py
+++ Day 9/Silent Auction.py
@@ -7,14 +7,24 @@ bids = {}
 bidding_finished = False
 
 def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ''
     for bidder in bidding_record:
-        bid
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print("The winner is ", winner," with a high bid of $", highest_bid)
+
+
 while not bidding_finished:
-    name = input("What is your name")
-    price = int(input("what is your bid? $"))
+    name = input("What is your name:")
+    price = int(input("what is your bid?: $"))
     bids[name] = price
-    should_continue = input("Are there any other bidders? 'yes' or 'no'")
+    should_continue = input("Are there any other bidders? 'yes' or 'no' \n")
     if should_continue.lower() =="no":
         bidding_finished = True
+        find_highest_bidder(bids)
     elif should_continue.lower() == "yes":
         clear()
+        
